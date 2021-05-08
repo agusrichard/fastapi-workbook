f1fdb12e84f25b6637125ec683c692ac579e3a97
agusrichard <agus.richard21@gmail.com>
2021-05-08 12:11:12 +0700

    recycle repository

--- FreeCodeCamp/JavaScript Algorithms and Data Structures Certification/learn.js
+++
@@ -1,8 +0,0 @@
-function telephoneCheck(str) {
-  // Good luck!
-  var regex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g;
-  return regex.test(str);
-}
-
-let result = telephoneCheck("555-555-5555");
-console.log(result);
