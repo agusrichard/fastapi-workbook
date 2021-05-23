5c0ad935668a16a61c195e853620687e46fc23be
agusrichard <agus.richard21@gmail.com>
2021-05-23 22:10:27 +0700

    for article

--- send-email/main.py
+++ send-email/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI, BackgroundTasks
 from send_email import send_email_background, send_email_async
 
-app = FastAPI()
+app = FastAPI(title='How to Send Email')
 
 
 @app.get('/')
@@ -12,16 +12,18 @@ def index():
 
 @app.get('/send-email/asynchronous')
 async def send_email_asynchronous():
-    await send_email_async('Hello World', 'richardagus921@gmail.com', {
-        'name': 'Hello World'
+    await send_email_async('Hello World', 'someemail@gmail.com', {
+        'title': 'Hello World',
+        'name': 'John Doe'
     })
     return 'Success'
 
 
 @app.get('/send-email/backgroundtasks')
 def send_email_backgroundtasks(background_tasks: BackgroundTasks):
-    send_email_background(background_tasks, 'Hello World', 'richardagus921@gmail.com', {
-        'name': 'Hello World'
+    send_email_background(background_tasks, 'Hello World', 'someemail@gmail.com', {
+        'title': 'Hello World',
+        'name': 'John Doe'
     })
     return 'Success'
 
