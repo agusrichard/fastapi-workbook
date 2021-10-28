2e49a625554bfada074ae540ba98829c4e4643e8
agusrichard <agus.richard21@gmail.com>
2021-10-29 04:23:59 +0700

    Staging

--- conductor-orchestration-main/main.py
+++ conductor-orchestration-main/main.py
@@ -1,13 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='Conductor Orchestraion Main')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
 def root():
-    return 'Conductor Orchestraion Main'
+    return {"message": "Sekardayu Hana Pradiani", "success": True}
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run('main:app', reload=True, port=8081)
